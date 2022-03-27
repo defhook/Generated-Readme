08a12ad9ac78bff16c3b9886dee4dee27d0b8fe7
Alvin Badillo <defhook152@gmail.com>
2022-03-27 15:18:23 -0700

    working returned licenses

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -8,16 +8,24 @@ function renderLicenseLink(license) { }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) { }
-
+function renderLicenseSection(licenses) {
+  let licenseSection = ''
+  if(licenses === 'None') {
+    licenseSection = ''
+  } else {
+    licenseSection = 
+    `Licenses: ${licenses}`
+  }
+  return licenseSection;
+}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return` 
 
   # ${data.title}
 
-  ## ${renderLicenseSection(data.license)} ${renderLicenseBadge(data.license)}
-  ### ${renderLicenseLink(data.license)}
+  ## ${renderLicenseSection(data.licenses)} ${renderLicenseBadge(data.licenses)}
+  ### ${renderLicenseLink(data.licenses)}
 
   ## Table of Contents:
   ###  * [Description](#description)
